f3cc1cd862b929896f6d9b9ea0b3a98d60f80319
Richard Frank <richardfrk@gmail.com>
2016-10-25 15:44:58 -0200

    First Commit by XCode

---
+++ Extension/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  Extension
+//
+//  Created by Richard Frank on 25/10/16.
+//  Copyright © 2016 Richard Frank. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
